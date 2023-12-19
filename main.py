b1dd161b4785166e6312763a9711311742af5acc
watchakorn-18k <74919942+watchakorn-18k@users.noreply.github.com>
2023-12-20 02:00:47 +0700

    create file

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
+"""
+
 ออกแบบโปรแกรมรับข้อมูลจากผู้ใช้งานเป็นตัวเลขจากนั้นตรวจสอบดังนี้
 - ถ้าเลขเป็นบวก แสดง The number you entered is positive
 - ถ้าเลขเป็นลบ แสดง The number you entered is negative
 - ถ้าเลขเป็นศูนย์ แสดง The number you entered is zero
 
+
+"""
