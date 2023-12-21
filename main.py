44fa85964ac564edff54f55edac974fcf9ca871e
andythongkipt <andythongkipt26@gmail.com>
2023-12-21 02:04:12 -0500

    fix: succes code

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
 """
-
 ออกแบบโปรแกรมรับข้อมูลจากผู้ใช้งานเป็นตัวเลขจากนั้นตรวจสอบดังนี้
 - ถ้าเลขเป็นบวก แสดง The number you entered is positive
 - ถ้าเลขเป็นลบ แสดง The number you entered is negative
 - ถ้าเลขเป็นศูนย์ แสดง The number you entered is zero
 
+""" 
+num_1 = int(input("Enter number : "))
+if num_1 > 0 :
+    print("The number you entered is positive")
+elif num_1 < 0 :
+    print("The number you entered is negative")
+else:
+    print("The number you entered is zero")
 
-"""
+git config --global user.email "andythongkipt26@gmail.com"
+git config --global user.name "andythongkipt"
